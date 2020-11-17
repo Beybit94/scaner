62a4bf35f3ba64b3ef72dd6c4a4c01b92deee621
beka.brother <aka.beka94@mail.ru>
2020-11-17 17:29:57 +0600

    stable version

--- src/services/api/Constants.ts
+++ src/services/api/Constants.ts
@@ -1,7 +1,7 @@
 export enum Endpoints {
-  BASE = "http://89.20.48.50:3365",
+  //BASE = "http://89.20.48.50:3365",
   //BASE = "http://10.20.254.5:44341",
-  //BASE = "http://192.168.0.169:44341",
+  BASE = "http://192.168.0.169:44341",
   TEST = "/api/User/TEST",
   LOGIN = "/api/User/FindUser",
   CREATE_TASK = "/api/Task/CreateTask",
