f6d6522a3b95cd0edafe5f73a415a4478b086cf0
beka.brother <aka.beka94@mail.ru>
2020-12-07 18:48:43 +0600

    Release 1.1.15

--- android/app/src/main/java/com/scanner/honeywel/HoneywellModule.java
+++ android/app/src/main/java/com/scanner/honeywel/HoneywellModule.java
@@ -85,8 +85,9 @@ public class HoneywellModule extends ReactContextBaseJavaModule implements Barco
                         reader.setProperty(BarcodeReader.PROPERTY_EAN_13_ENABLED, true);
                         reader.setProperty(BarcodeReader.PROPERTY_EAN_13_CHECK_DIGIT_TRANSMIT_ENABLED, true);
                         reader.setProperty(BarcodeReader.PROPERTY_UPC_A_ENABLE,true);
+                        reader.setProperty(BarcodeReader.PROPERTY_UPC_A_CHECK_DIGIT_TRANSMIT_ENABLED,true);
                         reader.setProperty(BarcodeReader.PROPERTY_UPC_A_NUMBER_SYSTEM_TRANSMIT_ENABLED,true);
-                        reader.setProperty(BarcodeReader.PROPERTY_UPC_A_TRANSLATE_EAN13,true);
+                        //reader.setProperty(BarcodeReader.PROPERTY_UPC_A_TRANSLATE_EAN13,true);
                         promise.resolve(true);
                     } catch (ScannerUnavailableException | UnsupportedPropertyException e) {
                         promise.resolve(false);
